a1062b701fe27274f1803af6da1ff421c455561f
Glorp <jonas.winje@gmail.com>
2024-11-17 15:15:01 +0100

    m

--- webby-lua-site/gcd.lua
+++
@@ -1,6 +0,0 @@
-local function gcd(a, b) return b == 0 and a or gcd(b, a % b) end
-
-return {
-  gcd = gcd
-}
-
--- webby-lua-site/lcd.lua
+++
@@ -1,9 +0,0 @@
-local gcd = require("gcd")
-local math = require("math")
-
-local function lcd(a, b) return math.abs(a * b) / gcd.gcd(a, b) end
-
-return {
-  lcd = lcd
-}
-
